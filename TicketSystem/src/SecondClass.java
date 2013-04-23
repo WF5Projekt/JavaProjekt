c17fbd73e079f6d78755e8dd28d0abb0a9fa1843
MatzeLi <Mathias.Lipke@web.de>
2013-04-23 14:47:37 +0200

    changed class 2

--- TicketSystem/src/SecondClass.java
+++ TicketSystem/src/SecondClass.java
@@ -1,12 +1,9 @@
 
 public class SecondClass {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("HAha");
 	}
 
 }
